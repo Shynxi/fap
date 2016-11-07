64230739f41fc2ddb9771ece56f7a57b7296aa11
Tobias Lengsholz <t.lengsholz@neusta.de>
2016-11-07 06:31:46 +0100

    included sass support via style modules

---
+++ gulpfile.js/tasks/stylemod.js
@@ -0,0 +1,49 @@
+var config       = require('../config')
+var changed      = require('gulp-changed')
+var gulp         = require('gulp')
+var path         = require('path')
+var stylemod     = require('gulp-style-modules')
+var gulpif       = require('gulp-if')
+var browserSync  = require('browser-sync')
+var sass         = require('gulp-sass')
+var sourcemaps   = require('gulp-sourcemaps')
+var handleErrors = require('../lib/handleErrors')
+var autoprefixer = require('gulp-autoprefixer')
+var cssnano      = require('gulp-cssnano')
+
+
+var paths = {
+  src: [
+    path.join(config.root.src, config.tasks.webcomponents.src, '/**/*.sass'),
+    path.join('!' + config.root.src, config.tasks.webcomponents.src, '/README.md')
+  ],
+  dest: path.join(config.root.dest, config.tasks.webcomponents.dest)
+}
+
+
+var stylemodTask = function () {
+  return gulp.src(paths.src)
+  .pipe(changed(paths.dest)) // Ignore unchanged files
+  .pipe(sass(config.tasks.css.sass))
+  .on('error', handleErrors)
+  .pipe(autoprefixer(config.tasks.css.autoprefixer))
+  .pipe(gulpif(global.production, cssnano({autoprefixer: false})))
+  .pipe(gulpif(!global.production, sourcemaps.write()))
+  .pipe(stylemod({
+    filename: function(file) {
+      //unique component name is name of last folder
+      var componentName = path.dirname(file.path).split(path.sep).pop()
+      return componentName + "-" + path.basename(file.path, path.extname(file.path));
+    },
+    moduleId: function (file) {
+      //unique component name is name of last folder
+      var componentName = path.dirname(file.path).split(path.sep).pop()
+      return componentName + "-" + path.basename(file.path, path.extname(file.path));
+    }
+  }))
+  .pipe(gulp.dest(paths.dest))
+  .pipe(browserSync.stream())
+}
+
+gulp.task('stylemod', stylemodTask)
+module.exports = stylemodTask
--- gulpfile.js/tasks/webcomponents.js
+++ gulpfile.js/tasks/webcomponents.js
@@ -1,29 +1,33 @@
-var config  = require('../config')
+var config = require('../config')
 var changed = require('gulp-changed')
-var gulp    = require('gulp')
-var path    = require('path')
+var gulp = require('gulp')
+var path = require('path')
 var vulcanize = require('gulp-vulcanize')
+var handleErrors = require('../lib/handleErrors')
+var gulpif       = require('gulp-if')
+var htmlmin      = require('gulp-htmlmin')
 
 var paths = {
-    src: [
-        path.join(config.root.src, config.tasks.webcomponents.src, '/**'),
-        path.join('!' + config.root.src, config.tasks.webcomponents.src, '/README.md')
-    ],
-    dest: path.join(config.root.dest, config.tasks.webcomponents.dest)
+  src: [
+    path.join(config.root.src, config.tasks.webcomponents.src, '/**'),
+    path.join('!' + config.root.src, config.tasks.webcomponents.src, '/README.md')
+  ],
+  dest: path.join(config.root.dest, config.tasks.webcomponents.dest)
 }
 
-var webcomponentsTask = function() {
-    return gulp.src(paths.src)
-        .pipe(changed(paths.dest)) // Ignore unchanged files
-        .pipe(vulcanize({
-            abspath: '',
-            excludes: [],
-            stripComments: true,
-            inlineScripts: true,
-            inlineCss: true,
-            stripExcludes: false
-        }))
-        .pipe(gulp.dest(paths.dest))
+var webcomponentsTask = function () {
+  return gulp.src(paths.src)
+  .pipe(vulcanize({
+    abspath: '',
+    excludes: [],
+    stripComments: true,
+    inlineScripts: true,
+    inlineCss: true,
+    stripExcludes: false
+  }))
+  .on('error', handleErrors)
+  .pipe(gulpif(global.production, htmlmin(config.tasks.html.htmlmin)))
+  .pipe(gulp.dest(paths.dest))
 }
 
 gulp.task('webcomponents', webcomponentsTask)
